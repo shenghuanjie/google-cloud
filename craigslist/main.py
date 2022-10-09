21abbb1a4a96f46ba252b9ffb6824a3877316b75
shengh4 <huanjie.sheng@roche.com>
2022-10-09 10:26:17 -0700

    limit network speed

--- craigslist/main.py
+++ craigslist/main.py
@@ -120,6 +120,7 @@ def web_loader(url, browser=None):
     # logger.info(os.environ['PATH'])
     # binary = FirefoxBinary(os.path.join(firefox_path, 'firefox.exe'))
     # browser = webdriver.Firefox(firefox_binary=binary)
+    os.system('sudo wondershaper ens4 10 9999')
     try:
         if browser is None:
             closer_browser = True
@@ -162,6 +163,7 @@ def web_loader(url, browser=None):
     finally:
         if browser is not None:
             browser.quit()
+        os.system('sudo wondershaper ens4 1 9999')
     return page_source
 
 
